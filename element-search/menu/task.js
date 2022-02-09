a6557b4be46ac1639b8515731af57337054b73a6
gfjdfeiw938cj <artyrgame@mail.ru>
2022-02-09 22:32:04 +0300

    на проверке

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,9 +1,25 @@
 
-document.addEventListener("click", function(event){
-   let target = event.target;
-   target.nextElementSibling.classList.toggle('menu_active')
-   event.preventDefault()
-}) 
+let element = document.querySelector('.menu_main');
+
+element.addEventListener("click", function(event){
+    let target = event.target;
+    target.nextElementSibling.classList.toggle('menu_active');
+
+    element.addEventListener("click", function(event){
+        let target = event.target.innerHTML;
+        let menu = element.querySelector('.menu__item').nextElementSibling.querySelector('.menu_sub').getAttribute('class'); 
+        let menu1 = element.querySelector('.menu__item').nextElementSibling.nextElementSibling.querySelector('.menu_sub').getAttribute('class');
+        if(menu == menu1){
+            if(target == "О компании"){        
+              element.querySelector('.menu__item').nextElementSibling.nextElementSibling.querySelector('.menu_sub').classList.remove('menu_active');
+            }else if(target == "Услуги"){
+              element.querySelector('.menu__item').nextElementSibling.querySelector('.menu_sub').classList.remove('menu_active');
+            }
+        }
+    })
+
+    event.preventDefault();
+ });
 
 
 
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,26 +2,65 @@ const slides = document.querySelectorAll('.slider__item');
 const prev = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 
-let slideNum = 0;
-
-next.onclick = function () {
-    slideNum++;
-    for (let slide of Array.from(slides)) {
-        slide.className = 'slider__item';
-    }
-    if (slideNum >= slides.length) {
-        slideNum = 0;
-    }
-    slides[slideNum].className = 'slider__item slider__item_active';
+next.onclick = function(){
+    
+    let slide = Array.from(slides).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
+    
+    if (slide >= slides.length - 1) {
+        slides[slide].classList.remove('slider__item_active')
+        slide = 0;
+        slides[slide].classList.add('slider__item_active') 
+    }else{
+        slides[slide].classList.remove('slider__item_active')
+        slides[slide+1].classList.add('slider__item_active')  
+    } 
 };
 
 prev.onclick = function () {
-    slideNum--;
-    for (let slide of Array.from(slides)) {
-        slide.className = 'slider__item';
+   
+    let slide = Array.from(slides).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
+
+    if (slide >= slides.length - 1) {
+        slides[slide].classList.remove('slider__item_active')
+        slide = 0;
+        slides[slide].classList.add('slider__item_active') 
+    }else{
+        slides[slide].classList.remove('slider__item_active')
+        slides[slide+1].classList.add('slider__item_active')  
     }
-    if (slideNum < 0) {
-        slideNum = slides.length - 1;
-    }
-    slides[slideNum].className = 'slider__item slider__item_active';
+
 };
+
+//                                      Второй вариант 
+
+// const slides = document.querySelectorAll('.slider__item');
+// const prev = document.querySelector('.slider__arrow_prev');
+// const next = document.querySelector('.slider__arrow_next');
+
+// let slideNum = 0;
+
+// console.log(Array.from(slides))
+
+// next.onclick = function(){
+//       slideNum++;
+//     for (let slide of Array.from(slides)) {
+//         slide.className = 'slider__item';
+//     }
+
+//     if (slideNum >= slides.length) {
+//         slideNum = 0;
+//     }
+//     slides[slideNum].className = 'slider__item slider__item_active';
+   
+// };
+
+// prev.onclick = function () {
+//     slideNum--;
+//     for (let slide of Array.from(slides)) {
+//         slide.className = 'slider__item';
+//     }
+//     if (slideNum < 0) {
+//         slideNum = slides.length - 1;
+//     }
+//     slides[slideNum].className = 'slider__item slider__item_active';
+// };
