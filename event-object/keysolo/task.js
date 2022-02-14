640542db6f1c2e3dfc7c859aa0768752c6438541
gfjdfeiw938cj <artyrgame@mail.ru>
2022-02-14 21:34:58 +0300

    44

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -1,11 +1,19 @@
 class Game {
   constructor(container) {
     this.container = container;
-    console.log(this.container)
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
-
+//----------------------------------------------------------------------------
+//                             Настройки для таймера
+   
+    this.formsTimer();
+    this.TIME_LIMIT = 20;
+    this.timePassed = 0;
+    this.timeLeft = TIME_LIMIT;
+    this.timerInterval = null;
+    this.startTimer();
+//----------------------------------------------------------------------------
     this.reset();
 
     this.registerEvents();
@@ -18,6 +26,7 @@ class Game {
   }
 
   registerEvents() {
+    
     document.addEventListener('keydown', function(event){ 
       const eventLetter = event.key.toLowerCase().charCodeAt(0);
       const screenLetter = this.currentSymbol.textContent.toLowerCase().charCodeAt(0);
@@ -26,10 +35,75 @@ class Game {
       } else {
           this.fail();
       }
-    }); 
-      
+    });
+  }
+//----------------------------------------------------------------------
+//                                     Методы для таймера 
+  formsTimer(){
+    document.getElementById("app").innerHTML = `
+        <div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining green"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
   }
 
+  startTimer(){
+    this.timerInterval = setInterval(() => {
+      // Количество времени, которое прошло, увеличивается на  1
+          this.timePassed = this.timePassed += 1;
+          this.timeLeft = this.TIME_LIMIT - this.timePassed;
+      // Обновляем метку оставшегося времени
+          document.getElementById("base-timer-label").innerHTML = formatTime(this.timeLeft);
+          setCircleDasharray();
+          // setRemainingPathColor(timeLeft);
+      
+          if (timeLeft === 0) {
+            onTimesUp();
+          }
+        }, 1000);
+  }
+
+  formatTime(time){
+    const minutes = Math.floor(time / 60); // Наибольшее целое число меньше или равно результату деления времени на 60.
+ 
+    let seconds = time % 60; // Секунды – это остаток деления времени на 60 (оператор модуля)
+
+    if (seconds < 10) {
+    seconds = `0${seconds}`;
+    }
+
+    return `${minutes}:${seconds}`;
+  }
+
+  calculateTimeFraction(){
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+  }
+
+  setCircleDasharray(){
+    const circleDasharray = `${(calculateTimeFraction() * 283).toFixed(0)} 283`;
+    document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", circleDasharray);
+  }
+
+//-----------------------------------------------------------------------
   success() {
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
@@ -91,4 +165,3 @@ class Game {
 }
 
 new Game(document.getElementById('game'))
-
