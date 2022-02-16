98e271e46aea96a96a407ee749578c3d99797bf7
gfjdfeiw938cj <artyrgame@mail.ru>
2022-02-16 22:10:28 +0300

    Требует проверки

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -4,19 +4,17 @@ class Game {
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
-//----------------------------------------------------------------------------
-//                             Настройки для таймера
-   
-    this.formsTimer();
-    this.TIME_LIMIT = 20;
-    this.timePassed = 0;
-    this.timeLeft = TIME_LIMIT;
-    this.timerInterval = null;
-    this.startTimer();
-//----------------------------------------------------------------------------
-    this.reset();
 
-    this.registerEvents();
+    this.reset();
+    
+    this.TIME_LIMIT = document.querySelectorAll('.symbol').length 
+    this.timeLeft = this.TIME_LIMIT
+    this.timePassed = 0;
+    this.timerInterval = null;
+    this.formsTimer();
+
+    this.registerEvents(); 
+
   }
 
   reset() {
@@ -25,66 +23,66 @@ class Game {
     this.lossElement.textContent = 0;
   }
 
-  registerEvents() {
-    
-    document.addEventListener('keydown', function(event){ 
+  async registerEvents() {
+     
+      document.addEventListener('keydown', event => {
+      if(this.winsElement.textContent == 0 && this.lossElement.textContent == 0 && document.querySelectorAll('.symbol')[0].innerHTML == this.currentSymbol.textContent.toLowerCase()){
+        this.startTimer(); 
+      }
       const eventLetter = event.key.toLowerCase().charCodeAt(0);
       const screenLetter = this.currentSymbol.textContent.toLowerCase().charCodeAt(0);
       if (screenLetter === eventLetter) {
           this.success();
       } else {
-          this.fail();
+          this.fail();        
       }
-    });
+      });
   }
-//----------------------------------------------------------------------
-//                                     Методы для таймера 
+                                
   formsTimer(){
     document.getElementById("app").innerHTML = `
-        <div class="base-timer">
-  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <g class="base-timer__circle">
-      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-      <path
-        id="base-timer-path-remaining"
-        stroke-dasharray="283"
-        class="base-timer__path-remaining green"
-        d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-      ></path>
-    </g>
-  </svg>
-  <span id="base-timer-label" class="base-timer__label">${formatTime(
-    timeLeft
-  )}</span>
-</div>
+    <div class="base-timer">
+    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <g class="base-timer__circle">
+        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+        <path
+          id="base-timer-path-remaining"
+          stroke-dasharray="283"
+          class="base-timer__path-remaining green"
+          d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "
+        ></path>
+      </g>
+    </svg>
+    <span id="base-timer-label" class="base-timer__label">${this.formatTime(
+      this.timeLeft
+    )}</span>
+  </div>
 `;
   }
 
   startTimer(){
-    this.timerInterval = setInterval(() => {
-      // Количество времени, которое прошло, увеличивается на  1
+     this.timerInterval = setInterval(() => {
           this.timePassed = this.timePassed += 1;
           this.timeLeft = this.TIME_LIMIT - this.timePassed;
-      // Обновляем метку оставшегося времени
-          document.getElementById("base-timer-label").innerHTML = formatTime(this.timeLeft);
-          setCircleDasharray();
-          // setRemainingPathColor(timeLeft);
-      
-          if (timeLeft === 0) {
-            onTimesUp();
-          }
+    
+          document.getElementById("base-timer-label").innerHTML = this.formatTime(this.timeLeft);
+          this.setCircleDasharray();
+
+          if (this.timeLeft === 0) {
+            this.fail()
+          }      
         }, 1000);
   }
 
   formatTime(time){
-    const minutes = Math.floor(time / 60); // Наибольшее целое число меньше или равно результату деления времени на 60.
+    const minutes = Math.floor(time / 60); 
  
-    let seconds = time % 60; // Секунды – это остаток деления времени на 60 (оператор модуля)
+    let seconds = time % 60; 
 
     if (seconds < 10) {
     seconds = `0${seconds}`;
@@ -93,17 +91,31 @@ class Game {
     return `${minutes}:${seconds}`;
   }
 
+  onTimesUp() {
+    clearInterval(this.timerInterval);
+  }
+
   calculateTimeFraction(){
-    const rawTimeFraction = timeLeft / TIME_LIMIT;
-    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    const rawTimeFraction = this.timeLeft / this.TIME_LIMIT;
+    return rawTimeFraction - (1 / this.TIME_LIMIT) * (1 - rawTimeFraction);
   }
 
   setCircleDasharray(){
-    const circleDasharray = `${(calculateTimeFraction() * 283).toFixed(0)} 283`;
+    const circleDasharray = `${(this.calculateTimeFraction() * 283).toFixed(0)} 283`;
     document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", circleDasharray);
   }
 
-//-----------------------------------------------------------------------
+  subsequentStartTimerFunctions(){
+
+    this.TIME_LIMIT = document.querySelectorAll('.symbol').length 
+    this.timeLeft = this.TIME_LIMIT;
+    this.timePassed = 0;
+    this.timerInterval = null;
+    this.formsTimer();
+    this.onTimesUp();
+    this.startTimer()
+  }
+
   success() {
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
@@ -115,6 +127,9 @@ class Game {
       alert('Победа!');
       this.reset();
     }
+    this.onTimesUp();
+    this.subsequentStartTimerFunctions()
+
     this.setNewWord();
   }
 
@@ -123,6 +138,9 @@ class Game {
       alert('Вы проиграли!');
       this.reset();
     }
+    this.onTimesUp();
+    this.subsequentStartTimerFunctions()
+
     this.setNewWord();
   }
 
