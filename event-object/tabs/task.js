91c1b3853e8cdfe8bb61ee50d384691983c5b7d2
gfjdfeiw938cj <artyrgame@mail.ru>
2022-02-17 18:38:41 +0300

    проверка

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,20 +1,14 @@
 const tabs = document.querySelectorAll('.tab');
 const contentList = document.querySelectorAll('.tab__content');
 
-for (let tab of tabs) {
-    tab.addEventListener('click', change)
-}
+tabs.forEach(el => el.addEventListener('click', change));
 
 function change(event) {
-
-    for (let tab of tabs) {
-        tab.className = 'tab';
-    }
-    event.target.className = 'tab tab_active';
-
-    for (let article of contentList) {
-        article.className = 'tab__content';
-    }
+    
+    tabs.forEach(el => el.className = 'tab');
+    event.target.classList.add('tab_active');
+    contentList.forEach(el => el.className = 'tab__content');
     const index = Array.from(tabs).indexOf(event.target);
-    contentList[index].className = 'tab__content tab__content_active';
+    contentList[index].classList.add('tab__content_active');
+ 
 }
