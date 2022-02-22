fef4dba74602ae1bd08db99d752b0c1317ebb49a
gfjdfeiw938cj <artyrgame@mail.ru>
2022-02-22 20:10:51 +0300

    Проверка

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,36 +1,17 @@
-let menu = document.querySelectorAll('.menu_sub')
 
-let menuLink = document.querySelectorAll('.menu__link');
+const links = Array.from(document.querySelectorAll('.menu__link'));
 
-
-console.log(menuLink)
-
-Array.from(menuLink).forEach((el,ind) => el.onclick = () => checkMenu(ind)) 
-
-function checkMenu(ind) {
-	let parent = menuLink[ind].parentElement.querySelector('.menu_sub');
-
-	console.log(parent)
-
-	if (parent){
-		change(menuLink[ind]);
-
-		if (parent.className.includes('menu_active')){
-			parent.classList.remove('menu_active');
+for (let link of links) {
+	link.onclick = function () {
+		const parent = link.parentElement;
+		if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
+			parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
 		} else {
-			parent.classList.add('menu_active');
+			parent.querySelector('.menu_sub').className = 'menu menu_sub'
+		}
+
+		if (link.closest('.menu_main')) {
+			return false
 		}
 	}
-	
-	return false;
 }
-
-function change(element) {
-	console.log(element)
-	let elementActive = element.closest('.menu_main').querySelector('.menu_active');
-	if(elementActive) {
-		elementActive.classList.remove('menu_active');
-	}
-	return false;
-}
-
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,8 +1,8 @@
 let sliderArrow = document.querySelectorAll('.slider__arrow');
 let sliderItem = document.querySelectorAll('.slider__item');
 let sliderDot = document.querySelectorAll('.slider__dot');
-const pointsArr = document.querySelectorAll('.slider__dot');
-const slides = document.querySelectorAll('.slider__item');
+let pointsArr = document.querySelectorAll('.slider__dot');
+let slides = document.querySelectorAll('.slider__item');
 pointsArr[0].classList.add('slider__dot_active')
 Array.from(sliderArrow).forEach(el => el.onclick = changeSlide); 
 
@@ -43,12 +43,6 @@ function changeSlide(event) {
 		
 }
 
-// function dotSlide(key) {
-// 	sliderItem[index].classList.remove('slider__item_active');
-// 	sliderItem[key].classList.add('slider__item_active')
-// 	index = key;
-// }
-
 Array.from(pointsArr).forEach(el => el.addEventListener("click" , function(event){
 	if(Array.from(pointsArr).some(el => el.getAttribute("class") == "slider__dot slider__dot_active")){
         let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == "slider__dot slider__dot_active") 
@@ -63,143 +57,5 @@ Array.from(pointsArr).forEach(el => el.addEventListener("click" , function(event
 
 
 
-
-
-//--------------------------------------------------------------------------
-// const slides = document.querySelectorAll('.slider__item'); 
-// console.log(slides)
-// const pointsArr = document.querySelectorAll('.slider__dot')
-// console.log(pointsArr)
-// const prev = document.querySelector('.slider__arrow_prev');
-// console.log(prev)
-// const next = document.querySelector('.slider__arrow_next');
-// console.log(next)
-// const points = document.querySelector('.slider__dots')
-// console.log(points)
-
-
-// next.onclick = function(){
-    
-//     let slide = Array.from(slides).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
-    
-//     if (slide >= slides.length - 1) {
-//         slides[slide].classList.remove('slider__item_active')
-//         slide = 0;
-//         slides[slide].classList.add('slider__item_active') 
-//     }else{
-//         slides[slide].classList.remove('slider__item_active')
-//         slides[slide+1].classList.add('slider__item_active')  
-//     } 
-// };
-
-// prev.onclick = function () {
-   
-//     let slide = Array.from(slides).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
-
-//     if (slide >= slides.length - 1) {
-//         slides[slide].classList.remove('slider__item_active')
-//         slide = 0;
-//         slides[slide].classList.add('slider__item_active') 
-//     }else{
-//         slides[slide].classList.remove('slider__item_active')
-//         slides[slide+1].classList.add('slider__item_active')  
-//     }
-// };
-
-// points.addEventListener("click" , function(event){
-//     let target = event.target
-//     if(target.getAttribute("class") != 'slider__dot'){
-//         return 
-//     } 
-//     targetElement(target)
-
-// })
-
-//  function targetElement(target){
-
-//     if( Array.from(pointsArr).some(el => el.getAttribute("class") == "slider__dot slider__dot_active")){
-//         let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == "slider__dot slider__dot_active") 
-//         pointsArr[pointInd].classList.remove('slider__dot_active')
-//     } 
-//     target.classList.add('slider__dot_active')
-//     let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == target.getAttribute("class")) 
-//     let slideInd = Array.from(slides).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
-//     slides[slideInd].classList.remove('slider__item_active')
-//     slides[pointInd].classList.add('slider__item_active')
-//  }
-
-//---------------------------------------------------------------------------------
-
-// const prevNextArr = document.querySelectorAll('.slider__arrow');
-// console.log(prevNextArr);
-// const slides = document.querySelectorAll('.slider__item');
-// console.log(slides);
-// const pointsArr = document.querySelectorAll('.slider__dot');
-// console.log(pointsArr);
-
-
-// Array.from(prevNextArr).forEach( el => el.addEventListener("click" , function(){
-// 	let slide = Array.from(slides).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
-//     console.log(slide)
-// 	    if (slide >= slides.length - 1) {
-// 	        slides[slide].classList.remove('slider__item_active')
-// 	        slide = 0;
-// 	        slides[slide].classList.add('slider__item_active') 
-// 	    }else{
-// 	        slides[slide].classList.remove('slider__item_active')
-// 	        slides[slide+1].classList.add('slider__item_active')  
-// 	    } 
-
-		// if(Array.from(pointsArr).some(el => el.getAttribute("class") == "slider__dot slider__dot_active")){
-		// 	// let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == "slider__dot slider__dot_active") 
-		// 	if(pointsArr[slide - 1].classList.contains("slider__dot_active")){
-		// 		pointsArr[slide - 1].classList.remove('slider__dot_active');  
-		// 		pointsArr[slide].classList.add('slider__dot_active');
-				
-		// 	}else if(pointsArr[slide + 1].classList.contains("slider__dot_active") ){
-		// 		pointsArr[slide + 1].classList.remove('slider__dot_active');  
-		// 		pointsArr[slide].classList.add('slider__dot_active');
-		// 	}
-		// }
-		
-		// pointsArr[slide].classList.add('slider__dot_active'); 
-	
-// }))
-
-// points.addEventListener("click" , function(event){
-//     let target = event.target
-//     if(target.getAttribute("class") != 'slider__dot'){
-//         return 
-//     } 
-//     targetElement(target)
-
-// })
-
-//  function targetElement(target){
-
-//     if(Array.from(pointsArr).some(el => el.getAttribute("class") == "slider__dot slider__dot_active")){
-//         let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == "slider__dot slider__dot_active") 
-//         pointsArr[pointInd].classList.remove('slider__dot_active')
-//     } 
-//     target.classList.add('slider__dot_active')
-//     let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == target.getAttribute("class")) 
-//     let slideInd = Array.from(slides).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
-//     slides[slideInd].classList.remove('slider__item_active')
-//     slides[pointInd].classList.add('slider__item_active')
-//  }
-
-
-// Array.from(pointsArr).forEach(el => el.addEventListener("click" , function(event){
-// 	if(Array.from(pointsArr).some(el => el.getAttribute("class") == "slider__dot slider__dot_active")){
-//         let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == "slider__dot slider__dot_active") 
-//         pointsArr[pointInd].classList.remove('slider__dot_active')
-// 	}
-//     console.log(event.target)
-// 	event.target.classList.add('slider__dot_active')
-// 	let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == event.target.getAttribute("class")) 
-//     let slideInd = Array.from(slides).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
-//     slides[slideInd].classList.remove('slider__item_active')
-//     slides[pointInd].classList.add('slider__item_active')
-// }))
 
 
