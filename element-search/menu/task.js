9208489aa81436750b90c54ff17f5589464886e9
gfjdfeiw938cj <artyrgame@mail.ru>
2022-02-24 18:11:25 +0300

    проверка

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,23 +1,19 @@
-let a = document.querySelectorAll('.menu__item')
-const links = Array.from(document.querySelectorAll('.menu__link'));
-
-console.log(links)
-
-const lin = Array.from(a).filter(el => el.closest('.menu_sub') != null )
-console.log(lin)
-
-for (let link of lin) {
+for (let link of links) {
 	console.log(link)
 	link.onclick = function () {
 		const parent = link.parentElement;
-		if (parent.querySelector('.menu_sub').className == 'menu menu_sub') {
-			parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
-		} else {
+	
+		if(parent.querySelector('.menu_sub') != null) { 
+		    if (parent.querySelector('.menu_sub').className == 'menu menu_sub') {
+			    parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+		    }else{
 			parent.querySelector('.menu_sub').className = 'menu menu_sub'
-		}
+		    }
 
-		if (link.closest('.menu_main')) {
-			return false
-		}
+		    if (link.closest('.menu_main')) {
+			    return false
+		    }
+	    }
 	}
+	
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,36 +3,34 @@ let sliderItem = document.querySelectorAll('.slider__item');
 let pointsArr = document.querySelectorAll('.slider__dot');
 
 pointsArr[0].classList.add('slider__dot_active')
+
 Array.from(sliderArrow).forEach(el => el.onclick = changeSlide); 
 
 Array.from(pointsArr).forEach((el,ind) => el.onclick = () => dotSlide(ind));
 
 function changeSlide(event) {
 	let classSlideArrow = event.target.classList;
-	let slide  = pointСhoice() 
-
-	sliderItem[slide].classList.remove('slider__item_active');
+	let slideInd  = pointСhoice() 
 
 	if(classSlideArrow.contains('slider__arrow_next')) {
-		if (slide == sliderItem.length - 1) {
-			slide = 0;
+		if (slideInd == sliderItem.length - 1) {
+			slideInd = 0;
 		} else {
-			slide++;
+			slideInd++;
 		}
 	}
 
 	if(classSlideArrow.contains('slider__arrow_prev')) {
-		if (slide == 0) {
-			slide = sliderItem.length - 1;
+		if (slideInd == 0) {
+			slideInd = sliderItem.length - 1;
 		} else {
-			slide--;
+			slideInd--;
 		}
 	}
-	sliderItem[slide].classList.add('slider__item_active');
+	sliderItem[slideInd].classList.add('slider__item_active');
 
-    pointsArr[slide].classList.add('slider__dot_active')
-    pointsArr[slide - 1].classList.remove('slider__dot_active')
-	
+    pointsArr[slideInd].classList.add('slider__dot_active')
+    pointsArr[slideInd - 1].classList.remove('slider__dot_active')
 		
 }
 
@@ -40,20 +38,17 @@ Array.from(pointsArr).forEach(el => el.addEventListener("click" , function(event
 	pointСhoice()
 	event.target.classList.add('slider__dot_active')
 	let pointInd = Array.from(pointsArr).findIndex(el => el.getAttribute("class") == event.target.getAttribute("class")) 
-    let slideInd = Array.from(sliderItem).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
-    sliderItem[slideInd].classList.remove('slider__item_active')
     sliderItem[pointInd].classList.add('slider__item_active')
 }))
 
 function pointСhoice(){
-	let slide = Array.from(sliderItem).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
+	let slideInd = Array.from(sliderItem).findIndex(el => el.getAttribute("class") == "slider__item slider__item_active")
 
 	if(Array.from(pointsArr).some(el => el.getAttribute("class") == "slider__dot slider__dot_active")){
-		pointsArr[slide].classList.toggle('slider__dot_active')
+		pointsArr[slideInd].classList.toggle('slider__dot_active')
 	}
-	return slide
+    sliderItem[slideInd].classList.remove('slider__item_active');
+	return slideInd
 }
-
-
 
 
