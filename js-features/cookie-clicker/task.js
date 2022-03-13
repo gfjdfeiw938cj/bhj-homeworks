48fed9d03a378e2c4c420265b78b0014873cf082
gfjdfeiw938cj <artyrgame@mail.ru>
2022-03-13 09:08:34 +0300

    Проверка

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,20 @@
 const counterClick = document.getElementById('clicker__counter');
-const speedCounter = document.getElementById('clicker__speed-counter');
 const images = document.getElementById('cookie');
+const clicker = document.querySelector('.clicker')
+const clickerSpeed = `
+      <div class="clicker__speed">
+        Скорость клика: <span id="clicker__speed-counter">0</span>
+      </div>
+            `;
+counterClick.insertAdjacentHTML("afterend", clickerSpeed);
+const speedCounter = document.getElementById('clicker__speed-counter');
 let counter = +counterClick.textContent;
 let oldDate = Date.now();
 
-images.onclick = () => {
-  counterClick.textContent = `${++counter}`;
-  speedCounter.textContent = (1000 / (Date.now() - oldDate)).toFixed(2);
-  oldDate = Date.now();
-  images.width = (counter % 2) ? 220 : 200;
-}
+  images.onclick = () => {
+    counterClick.textContent = `${++counter}`;
+    speedCounter.textContent = (1000 / (Date.now() - oldDate)).toFixed(2);
+    oldDate = Date.now();
+    images.width = (counter % 2) ? 220 : 200;
+  }
+
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -13,30 +13,52 @@ function checkGame(data) {
   const isLost = document.getElementById('lost');
   let isDeadCounter = +isDead.innerText;
   let isLostCounter = +isLost.innerText;
-  let oldId = null;   //Убираем двойной клик по элементу
+  let oldId = null;   
   
+  const holeGame = document.querySelectorAll('.hole');
   
-  
-  
-  for (let i = 1; i < 10; i++) {
-    document.getElementById(`hole${i}`).onclick = function () {
-      if (this.className.includes('hole_has-mole')) {
-        if (oldId !== i) {
-          isDead.innerText = `${++isDeadCounter}`;
-          //Закометировать
-          if (isDeadCounter === 10) checkGame(true);
-  
-          //Раскометировать
-          if (isDeadCounter === 10) interval = 700;
-          if (isDeadCounter === 20) interval = 600;
-          if (isDeadCounter === 30) interval = 550;
-          if (isDeadCounter === 40) interval = 500;
-  
-          oldId = i;
-        }
-      } else {
-        isLost.innerText = `${++isLostCounter}`;
-        if (isLostCounter === 5) checkGame(false)
+  Array.from(holeGame).forEach((el, ind) => el.addEventListener("click", function(){
+    let indNamber = ind + 1
+    if (this.className.includes('hole_has-mole')) {
+      if (oldId !== indNamber) {
+        isDead.innerText = `${++isDeadCounter}`;
+       
+        if (isDeadCounter === 10) checkGame(true);
+
+        if (isDeadCounter === 10) interval = 700;
+        if (isDeadCounter === 20) interval = 600;
+        if (isDeadCounter === 30) interval = 550;
+        if (isDeadCounter === 40) interval = 500;
+
+        oldId = indNamber;
       }
+    } else {
+      isLost.innerText = `${++isLostCounter}`;
+      if (isLostCounter === 5) checkGame(false)
     }
-  }
+
+  }))
+
+  //                              Или так (i < holeGame.length)
+
+  // for (let i = 1; i < holeGame.length; i++) {
+  //   document.getElementById(`hole${i}`).onclick = function () {
+  //     if (this.className.includes('hole_has-mole')) {
+  //       if (oldId !== i) {
+  //         isDead.innerText = `${++isDeadCounter}`;
+         
+  //         if (isDeadCounter === 10) checkGame(true);
+  
+  //         if (isDeadCounter === 10) interval = 700;
+  //         if (isDeadCounter === 20) interval = 600;
+  //         if (isDeadCounter === 30) interval = 550;
+  //         if (isDeadCounter === 40) interval = 500;
+  
+  //         oldId = i;
+  //       }
+  //     } else {
+  //       isLost.innerText = `${++isLostCounter}`;
+  //       if (isLostCounter === 5) checkGame(false)
+  //     }
+  //   }
+  // }
